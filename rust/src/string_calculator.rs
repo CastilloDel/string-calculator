16d951fe905e13756b02d575c56ef852070e3097
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-07 13:59:20 +0100

    Rust: Should parse if there is only one number

--- rust/src/string_calculator.rs
+++ rust/src/string_calculator.rs
@@ -1,5 +1,10 @@
-fn parseAndSum(input: &str) -> usize {
-    0
+use std::num::ParseIntError;
+
+fn parseAndSum(input: &str) -> Result<usize, ParseIntError> {
+    if input.is_empty() {
+        return Ok(0);
+    }
+    input.parse()
 }
 
 #[cfg(test)]
@@ -7,6 +12,11 @@ mod test {
     use super::*;
     #[test]
     fn should_return_0_if_input_is_empty() {
-        assert_eq!(parseAndSum(""), 0);
+        assert_eq!(parseAndSum(""), Ok(0));
+    }
+
+    #[test]
+    fn should_parse_if_there_is_only_one_number() {
+        assert_eq!(parseAndSum("4"), Ok(4));
     }
 }
